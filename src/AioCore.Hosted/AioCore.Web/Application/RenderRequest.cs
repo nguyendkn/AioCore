1a0bbad7ef5e917ee470cda0860e48bbd4ec8ab9
KhoiNguyenICT <khoinguyenict@gmail.com>
2022-03-17 17:08:05 +0700

    chore: config tailwind for aioc

--- src/AioCore.Hosted/AioCore.Web/Application/RenderRequest.cs
+++ src/AioCore.Hosted/AioCore.Web/Application/RenderRequest.cs
@@ -28,22 +28,22 @@ public class RenderRequest : IRequest<string>
 
         public async Task<string> Handle(RenderRequest request, CancellationToken cancellationToken)
         {
-            var templatePath = Path.Combine(_hostEnvironment.WebRootPath, "Templates", $"{request.Router}.liquid");
-            var templateValue = _memoryCache.Get<string>(templatePath);
-            if (string.IsNullOrEmpty(templateValue)) _memoryCache.Set(templatePath, templatePath.ReadFile());
-            templateValue = _memoryCache.Get<string>(templatePath);
+            var pagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Pages", $"{request.Router}.liquid");
+            var templateValue = _memoryCache.Get<string>(pagePath);
+            if (string.IsNullOrEmpty(templateValue)) _memoryCache.Set(pagePath, pagePath.ReadFile());
+            templateValue = _memoryCache.Get<string>(pagePath);
             var template = Template.Parse(templateValue);
             var output = await template.RenderAsync(new
             {
-                Title = "AioCore", Heading = "Scriban", Products = new List<string>
+                Title = "AioCore",
+                Heading = "Scriban",
+                Products = new List<string>
                 {
                     "xin chao",
                     "toi ten la Nguyen"
                 }
             });
-            var htmlMinifier = new HtmlMinifier();
-            var result = htmlMinifier.Minify(output, false);
-            return result.MinifiedContent;
+            return output;
         }
     }
 }

fdd61dc80d73907568f8145b1ace8595b88740e0
Dao Khoi Nguyen <nguyendk.on@gmail.com>
2022-08-23 23:11:55 +0700

    chore: create entity from selectbox create entity

--- src/AioCore.MediatR/AioCore.Write/SettingCommands/EntityCommands/SubmitEntityCommand.cs
+++ src/AioCore.MediatR/AioCore.Write/SettingCommands/EntityCommands/SubmitEntityCommand.cs
@@ -1,5 +1,6 @@
 using AioCore.Domain.DatabaseContexts;
 using AioCore.Domain.SettingAggregate;
+using AioCore.Shared.Common.Constants;
 using AioCore.Shared.SeedWorks;
 using MediatR;
 
@@ -7,6 +8,13 @@ namespace AioCore.Write.SettingCommands.EntityCommands;
 
 public class SubmitEntityCommand : SettingEntity, IRequest<Response<SettingEntity>>
 {
+    public SubmitEntityCommand(string? name)
+    {
+        Name = name;
+        CreatedAt = DateTime.Now;
+        ModifiedAt = DateTime.Now;
+    }
+
     internal class Handler : IRequestHandler<SubmitEntityCommand, Response<SettingEntity>>
     {
         private readonly SettingsContext _context;
@@ -16,9 +24,17 @@ public class SubmitEntityCommand : SettingEntity, IRequest<Response<SettingEntit
             _context = context;
         }
 
-        public Task<Response<SettingEntity>> Handle(SubmitEntityCommand request, CancellationToken cancellationToken)
+        public async Task<Response<SettingEntity>> Handle(SubmitEntityCommand request,
+            CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var entityEntry = await _context.Entities.AddAsync(request, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return new Response<SettingEntity>
+            {
+                Data = entityEntry.Entity,
+                Message = Messages.CreateDataSuccessful,
+                Success = true
+            };
         }
     }
 }
